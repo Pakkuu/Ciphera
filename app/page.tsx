483173cc07be0f96f1e103c4e3e8d37b86030472
pakkuu <ethan.pvarghese@gmail.com>
2024-06-10 18:25:02 -0500

    updated landing responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -15,8 +15,8 @@ const Landing = () => {
           objectFit="contain"
         />
       </div>
-      <div className="flex relative h-[85vh] w-[90%] bg-white bg-opacity-10 shadow-[0_0_15px_rgba(0,0,0,0.05)] backdrop-filter border-[#fffffff6] border-[0.06rem] backdrop-blur-[0.2rem] rounded-3xl m-auto z-10 items-center overflow-hidden">
-        <div className="w-[30rem] h-[15rem] relative opacity-80 ml-[200px]">
+      <div className="lg:flex lg:flex-row lg:justify-evenly sm:flex-col items-center relative h-[85vh] w-[90%] bg-white bg-opacity-10 shadow-[0_0_15px_rgba(0,0,0,0.05)] backdrop-filter border-[#fffffff6] border-[0.06rem] backdrop-blur-[0.2rem] rounded-3xl m-auto z-10 overflow-hidden">
+        <div className="w-[30rem] h-[15rem] relative opacity-80 lg:mx-0 sm:mx-auto">
           <Image
             src="/cyphera2.svg"
             alt="Cyphera Logo"
@@ -24,18 +24,18 @@ const Landing = () => {
             objectFit="contain"
           />
         </div>
-        <div className="flex-col mx-auto leading-[5rem] tracking-widest">
+        <div className="flex-col tracking-widest sm:text-center mt-4 lg:mt-0">
           <div className="text-[4rem] font-extralight opacity-70">
             Your Personal
           </div>
           <div className="text-[4rem] font-extralight opacity-70">
             Data Fortress
           </div>
-          <div className="text-[2rem] font-extralight opacity-20 mt-[-1rem]">
+          <div className="text-[2rem] font-extralight opacity-20">
             cloud storage - elegantly crafted
           </div>
           <Link href="/authenticate">
-            <Button className="font-space-mono rounded-[1rem] bg-[#353535bd] hover:bg-[#35353595] mt-[-2rem]" variant="contained" disableElevation>
+            <Button className="font-space-mono rounded-[1rem] bg-[#353535bd] hover:bg-[#35353595] mt-[1rem]" variant="contained" disableElevation>
               Get Started 
               <SlArrowRight className="ml-[1rem]"/>
             </Button>
